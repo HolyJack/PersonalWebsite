8378a05e920c52838db6ec517712b4c17818a3dc
holyjack <holyjackdev@gmail.com>
2023-06-04 16:10:56 +0400

    downloadable cv.pdf added

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,4 +1,16 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import ContactForm
 
 def main_page(request):
-    return render(request, "mainpage/index.html")
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            return HttpResponseRedirect(".")
+    
+    form = ContactForm()
+
+    
+    return render(request, "mainpage/index.html", {"form": form})
